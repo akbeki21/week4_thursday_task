e54c638749446f5e2c3e881843fb1df3b3ad4c1a
Beki <akmatovabeka21@gmail.com>
2020-11-07 23:20:22 +0600

    Task3

---
+++ task3.py
@@ -0,0 +1,37 @@
+# Task 1
+# def numbers(num):
+#     if num == 0:
+#         return[]
+#     else:
+#         return numbers(num - 1) + [num]
+
+# print(numbers(7))
+
+
+
+# Task 2
+# def sum(num):
+#     if num == 1:
+#         return num 
+#     return num + sum(num - 1)
+
+# print(sum(6))
+
+
+# Task 3
+# def reverse(num):
+#     return str(num)[::-1]
+
+# print(reverse(256))
+
+
+# Task 4
+# def fib_rec(num):
+#     if num <= 1:
+#         return num
+    
+#     return fib_rec(num - 1) + fib_rec(num - 2)
+
+# print(fib_rec(10))
+
+
